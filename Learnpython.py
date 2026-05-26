257fdb8aacdf30155334c52c62626952d053972c
PraveenKumar S <praveenkrsslm69@gmail.com>
2026-05-26 13:50:08 +0530

    Add files via upload

---
+++ Learnpython.py
@@ -0,0 +1,88 @@
+"""
+print("Welcome to Instagram❤")
+print("****Register Page****")
+#variable_name = data
+name = "Dhineesh" #string
+AGE = 2 #int
+City = 'Salem, Tamilnadu - 636001'
+phone_number = 9876543.21 #float
+name2 = "Kumar"
+check = True #boolean->(True/False)
+
+print("Name : ",name)
+print(type(name))
+print("AGE : ",AGE)
+print(type(AGE))
+print("CITY : ",City)
+print(type(City))
+print("Phone No : ",phone_number)
+print(type(phone_number))
+print("Nick Name : ",name2)
+print(type(name2))
+print("Age is Above 18 : ",check)
+print(type(check))
+"""
+#keywords
+#help("keywords")
+
+'''
+#input
+#1.compile time input
+name = "Dhineesh"
+#2.runtime input --> input()
+#type casting -> int(),float(),str(),bool()
+age = int(input("Enter Age : "))
+print(age,type(age))
+
+cgpa = float(input("Enter CGPA : "))
+print(cgpa,type(cgpa))
+'''
+
+#Operators
+#Arithmetic operator
+#  +,-,*,/,%,//,**
+a = 10
+b = 2
+print(a+b)#12
+print(a-b)#8
+print(a*b)#20
+print(a/b)#5.0
+print(a%b)#0
+print(a//b)#5
+c = a**b#100
+print(c)
+
+
+# find avg 3 numbers
+
+a = int(input("Enter a Value : "))
+b = int(input("Enter b Value : "))
+c = int(input("Enter c Value : "))
+avg = (a+b+c)/3
+print("Average :",avg)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

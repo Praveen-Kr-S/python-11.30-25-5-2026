c7fae3ccd2f554f59e010ccd97c2ee16b5323a51
PraveenKumar S <praveenkrsslm69@gmail.com>
2026-07-10 12:26:08 +0530

    Add files via upload

---
+++ .demo.py
@@ -0,0 +1,54 @@
+"""
+print("Hi")
+a = 10
+a = input("Enter a : ")
+b1 = input("Enter b : ")
+print("add value :
+      ",a+b)
+"""
+"""
+try:
+    #block of code
+except Exception as e:
+    #Show the exception
+"""
+
+#1
+'''
+try:
+    #ZeroDivisionError
+    a = 10
+    b = 0
+    print(a+b)
+    print(a/b)
+except Exception as e:
+    #print(e)
+    print("Zero Not Divisable")
+
+'''
+'''
+#2
+try:
+    #List Index out of range
+    #a = [11,22,33,45]
+    #print(a[8])
+    
+    #String Index out of range
+    #b = "Sugan"
+    #print(b[7])
+
+    #NameError
+    a = 10
+    print(a)
+    
+except Exception as e:
+    print(e)
+'''
+
+
+
+
+
+
+
+
